ae1520d12963382c8695eac9127b77ee81d4673b
jokesit <jokesittipong@gmail.com>
2026-06-03 14:09:33 +0700

    if else

---
+++ ifelfe.py
@@ -0,0 +1,175 @@
+# # 1. if แบบธรรมดา
+x = 4
+
+if x % 2 == 0:
+    print("even")
+else:
+    print("odd")
+    
+    
+    
+# # 2. if / elif / else
+# score = 75
+
+# if score >= 80:
+#     print("A")
+# elif score >= 70:
+#     print("B")
+# else:
+#     print("C")
+    
+    
+    
+# # 3. nested if คือ if ซ้อน if
+# age = 15
+# member = False
+
+# if age <= 18:
+#     if member == True:
+#         print(60)
+#     else:
+#         print(80)
+# else:
+#     if member == True:
+#         print(100)
+#     else:
+#         print(120)
+        
+        
+        
+        
+        
+# # ข้อ 1
+# x = 6
+
+# if x > 5:
+#     print("A")
+# else:
+#     print("B")
+    
+    
+# # ข้อ 2
+# x = 3
+
+# if x % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+    
+    
+# # ข้อ 3
+# x = 10
+
+# if x > 10:
+#     print("A")
+# else:
+#     print("B")
+    
+    
+# # ข้อ 4
+# x = 8
+
+# if x % 4 == 0:
+#     print("P")
+# else:
+#     print("Q")
+    
+    
+# # ข้อ 5
+# x = 7
+
+# if x % 2 == 1 and x > 5:
+#     print("Yes")
+# else:
+#     print("No")
+    
+    
+# # แบบฝึกหัด elif
+# # ข้อ 6
+# score = 85
+
+# if score >= 90:
+#     print("A")
+# elif score >= 80:
+#     print("B")
+# else:
+#     print("C")
+    
+    
+# # ข้อ 7
+# score = 70
+
+# if score > 70:
+#     print("A")
+# elif score == 70:
+#     print("B")
+# else:
+#     print("C")
+    
+    
+    
+# # ข้อ 8
+# x = 5
+
+# if x > 0:
+#     print("positive")
+# elif x == 0:
+#     print("zero")
+# else:
+#     print("negative")
+    
+    
+    
+# # แบบฝึกหัด nested if
+# # ข้อ 9
+# x = 10
+# y = 3
+
+# if x > 5:
+#     if y > 5:
+#         print("A")
+#     else:
+#         print("B")
+# else:
+#     print("C")
+    
+    
+    
+# # ข้อ 10
+# age = 18
+# member = True
+
+# if age <= 18:
+#     if member == True:
+#         print("60")
+#     else:
+#         print("80")
+# else:
+#     if member == True:
+#         print("100")
+#     else:
+#         print("120")
+        
+        
+        
+# # ข้อ 11
+# x = -2
+# y = -1
+
+# if x == y:
+#     print(x + y)
+# elif x > y:
+#     print(x - y)
+# else:
+#     print(y - x)
+    
+    
+    
+# # ข้อ 12
+# x = 4
+# y = 3
+
+# if x * y % 2 == 0:
+#     print("A")
+# else:
+#     print("B")
